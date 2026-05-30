a7a0b95360a464b579f87392b1b1e5e2bb05d66e
Justin Rietschel <justin.rietschel@stud.htw-dresden.de>
2026-05-30 13:48:41 +0200

    updated: service-worker.js

--- Src/service-worker.js
+++ Src/service-worker.js
@@ -10,24 +10,16 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
-
     event.waitUntil(
-
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(FILES_TO_CACHE);
-            })
+        caches.open(CACHE_NAME).then(cache =>
+            cache.addAll(FILES_TO_CACHE))
     );
 });
 
 self.addEventListener("fetch", event => {
-
     event.respondWith(
-
-        caches.match(event.request)
-            .then(response => {
-
-                return response || fetch(event.requent);
+        caches.match(event.request).then(response => {
+                return response || fetch(event.request);
             })
     );
 });
