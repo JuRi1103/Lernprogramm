b6dd9d1df2c7c93fc67b0797a2fe27116036351c
Justin Rietschel <justin.rietschel@stud.htw-dresden.de>
2026-06-05 16:26:54 +0200

    updated: service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -37,6 +37,11 @@ self.addEventListener("activate", event => {
 })
 
 self.addEventListener("fetch", event => {
+    if (event.request.url.endsWith("questions.json")) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+
     event.respondWith(
         caches.match(event.request).then(response => {
                 return response || fetch(event.request);
